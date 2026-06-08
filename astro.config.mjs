4ee768581e4d0f77f0a71338d0071addecf5dc2e
Wibaek Park <devmuromi@gmail.com>
2026-06-09 08:34:22 +0900

    chore: sitemap 생성 설정 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://minecraft-version-archive.pages.dev",
   output: "static",
+  integrations: [sitemap()],
   server: {
     port: 3162,
   },
